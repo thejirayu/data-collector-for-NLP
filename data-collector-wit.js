e702c8a5dd273d57507efa5538747cf04665dedc
thejirayu <jirayu.p.tni@gmail.com>
2017-11-23 06:02:26 -0500

    Update data-collector-wit.js

--- data-collector-wit.js
+++ data-collector-wit.js
@@ -1,8 +1,7 @@
 const {Wit, log} = require('node-wit');
-
 const client = new Wit({
   accessToken: 'IBKY5STM7CQEUH755KJZY35RB52S2MMP',
   logger: new log.Logger(log.DEBUG) // optional
 });
-
-console.log(client.message('none'));
+var args = process.argv.slice(2);
+console.log(client.message(args));
